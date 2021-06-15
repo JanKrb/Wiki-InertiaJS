eed547ea98e7cb9d6e4de3e3f25385cf5090be7c
Nino Gralla <nino.gralla@gmx.de>
2021-06-15 17:52:03 +0200

    update Profile view

--- Frontend/src/views/change-password/Main.vue
+++ Frontend/src/views/change-password/Main.vue
@@ -5,9 +5,7 @@
     </div>
     <div class="grid grid-cols-12 gap-6">
       <!-- BEGIN: Profile Menu -->
-      <div
-        class="col-span-12 lg:col-span-4 xxl:col-span-3 flex lg:block flex-col-reverse"
-      >
+      <div class="col-span-12 lg:col-span-4 xxl:col-span-3 flex lg:block flex-col-reverse">
         <div class="intro-y box mt-5">
           <div class="relative flex items-center p-5">
             <div class="w-12 h-12 image-fit">
@@ -23,124 +21,26 @@
               </div>
               <div class="text-gray-600">{{ $f()[0].jobs[0] }}</div>
             </div>
-            <div class="dropdown">
-              <a
-                class="dropdown-toggle w-5 h-5 block"
-                href="javascript:;"
-                aria-expanded="false"
-              >
-                <MoreHorizontalIcon
-                  class="w-5 h-5 text-gray-600 dark:text-gray-300"
-                />
-              </a>
-              <div class="dropdown-menu w-56">
-                <div class="dropdown-menu__content box dark:bg-dark-1">
-                  <div
-                    class="p-4 border-b border-gray-200 dark:border-dark-5 font-medium"
-                  >
-                    Export Options
-                  </div>
-                  <div class="p-2">
-                    <a
-                      href=""
-                      class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"
-                    >
-                      <ActivityIcon
-                        class="w-4 h-4 text-gray-700 dark:text-gray-300 mr-2"
-                      />
-                      English
-                    </a>
-                    <a
-                      href=""
-                      class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"
-                    >
-                      <BoxIcon
-                        class="w-4 h-4 text-gray-700 dark:text-gray-300 mr-2"
-                      />
-                      Indonesia
-                      <div
-                        class="text-xs text-white px-1 rounded-full bg-theme-6 ml-auto"
-                      >
-                        10
-                      </div>
-                    </a>
-                    <a
-                      href=""
-                      class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"
-                    >
-                      <LayoutIcon
-                        class="w-4 h-4 text-gray-700 dark:text-gray-300 mr-2"
-                      />
-                      English
-                    </a>
-                    <a
-                      href=""
-                      class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md"
-                    >
-                      <SidebarIcon
-                        class="w-4 h-4 text-gray-700 dark:text-gray-300 mr-2"
-                      />
-                      Indonesia
-                    </a>
-                  </div>
-                  <div
-                    class="px-3 py-3 border-t border-gray-200 dark:border-dark-5 font-medium flex"
-                  >
-                    <button type="button" class="btn btn-primary py-1 px-2">
-                      Settings
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-secondary py-1 px-2 ml-auto"
-                    >
-                      View Profile
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
           <div class="p-5 border-t border-gray-200 dark:border-dark-5">
-            <a
-              class="flex items-center text-theme-1 dark:text-theme-10 font-medium"
-              href=""
-            >
-              <ActivityIcon class="w-4 h-4 mr-2" /> Personal Information
-            </a>
-            <a class="flex items-center mt-5" href="">
-              <BoxIcon class="w-4 h-4 mr-2" /> Account Settings
-            </a>
-            <a class="flex items-center mt-5" href="">
-              <LockIcon class="w-4 h-4 mr-2" /> Change Password
-            </a>
-            <a class="flex items-center mt-5" href="">
-              <SettingsIcon class="w-4 h-4 mr-2" /> User Settings
-            </a>
+            <router-link :to="{ name: 'profile.personal' }">
+              <a class="flex items-center mt-5" href="">
+                <UserIcon class="w-4 h-4 mr-2"/> Personal Information
+              </a>
+            </router-link>
+            <router-link :to="{ name: 'profile.password' }">
+              <a class="flex items-center text-theme-1 dark:text-theme-10 font-medium" href="">
+                <LockIcon class="w-4 h-4 mr-2"/> Change Password
+              </a>
+            </router-link>
           </div>
           <div class="p-5 border-t border-gray-200 dark:border-dark-5">
             <a class="flex items-center" href="">
-              <ActivityIcon class="w-4 h-4 mr-2" /> Email Settings
+              <MailIcon class="w-4 h-4 mr-2"/> Email Settings
             </a>
             <a class="flex items-center mt-5" href="">
-              <BoxIcon class="w-4 h-4 mr-2" /> Saved Credit Cards
+              <InboxIcon class="w-4 h-4 mr-2"/> Social Networks
             </a>
-            <a class="flex items-center mt-5" href="">
-              <LockIcon class="w-4 h-4 mr-2" /> Social Networks
-            </a>
-            <a class="flex items-center mt-5" href="">
-              <SettingsIcon class="w-4 h-4 mr-2" /> Tax Information
-            </a>
-          </div>
-          <div class="p-5 border-t border-gray-200 dark:border-dark-5 flex">
-            <button type="button" class="btn btn-primary py-1 px-2">
-              New Group
-            </button>
-            <button
-              type="button"
-              class="btn btn-outline-secondary py-1 px-2 ml-auto"
-            >
-              New Quick Link
-            </button>
           </div>
         </div>
       </div>
@@ -154,42 +54,47 @@
             <h2 class="font-medium text-base mr-auto">Change Password</h2>
           </div>
           <div class="p-5">
-            <div>
-              <label for="change-password-form-1" class="form-label"
-                >Old Password</label
-              >
-              <input
-                id="change-password-form-1"
-                type="password"
-                class="form-control"
-                placeholder="Input text"
-              />
-            </div>
-            <div class="mt-3">
-              <label for="change-password-form-2" class="form-label"
-                >New Password</label
-              >
-              <input
-                id="change-password-form-2"
-                type="password"
-                class="form-control"
-                placeholder="Input text"
-              />
-            </div>
-            <div class="mt-3">
-              <label for="change-password-form-3" class="form-label"
-                >Confirm New Password</label
-              >
-              <input
-                id="change-password-form-3"
-                type="password"
-                class="form-control"
-                placeholder="Input text"
-              />
-            </div>
-            <button type="button" class="btn btn-primary mt-4">
-              Change Password
-            </button>
+            <form @submit.prevent="handleSubmit">
+              <div>
+                <label for="change-password-form-1" class="form-label">
+                  Old Password
+                </label>
+                <input
+                  id="change-password-form-1"
+                  type="password"
+                  class="form-control"
+                  placeholder="Input text"
+                  v-model="old_password"
+                />
+              </div>
+              <div class="mt-3">
+                <label for="change-password-form-2" class="form-label">
+                  New Password
+                </label>
+                <input
+                  id="change-password-form-2"
+                  type="password"
+                  class="form-control"
+                  placeholder="Input text"
+                  v-model="password"
+                />
+              </div>
+              <div class="mt-3">
+                <label for="change-password-form-3" class="form-label">
+                  Confirm New Password
+                </label>
+                <input
+                  id="change-password-form-3"
+                  type="password"
+                  class="form-control"
+                  placeholder="Input text"
+                  v-model="password_confirmation"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary mt-4">
+                Change Password
+              </button>
+            </form>
           </div>
         </div>
         <!-- END: Change Password -->
@@ -197,3 +102,36 @@
     </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      email: '',
+      old_password: '',
+      password: '',
+      password_confirmation: ''
+    }
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      axios.post('http://127.0.0.1:8000/api/auth/password/change', {
+        email: this.email,
+        old_password: this.old_password,
+        password: this.password,
+        password_confirmation: this.password_confirmation
+      })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.error(error.message)
+        })
+    }
+  }
+})
+</script>
