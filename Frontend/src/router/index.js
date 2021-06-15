be8d54b054c0136b62cc007506f32583d43a6cb1
Nino Gralla <nino.gralla@gmx.de>
2021-06-15 23:41:57 +0200

    edit Routes
    [+] Roles route

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import axios from 'axios'
 import TopMenu from '../layouts/top-menu/Main.vue'
 import DashboardOverview2 from '../views/dashboard-overview-2/Main.vue'
+import Roles from '../views/roles/Main.vue'
 import Login from '../views/login/Main.vue'
 import Register from '../views/register/Main.vue'
 import ErrorPage from '../views/error-page/Main.vue'
@@ -11,42 +13,103 @@ const routes = [
   {
     path: '/',
     component: TopMenu,
+    meta: {
+      title: 'Wiki',
+      auth: false,
+      isAuth: false
+    },
     children: [
       {
         path: 'dashboard',
         name: 'dashboard',
-        component: DashboardOverview2
+        component: DashboardOverview2,
+        meta: {
+          title: 'Dashboard',
+          auth: false,
+          isAuth: false
+        }
       },
       {
         path: 'profile',
         name: 'profile.personal',
-        component: UpdateProfile
+        component: UpdateProfile,
+        meta: {
+          title: 'Profile',
+          auth: true,
+          isAuth: false
+        }
       },
       {
         path: 'profile/change-password',
         name: 'profile.password',
-        component: ChangePassword
+        component: ChangePassword,
+        meta: {
+          title: 'Change Password',
+          auth: true,
+          isAuth: false
+        }
+      }
+    ]
+  },
+  {
+    path: '/admin',
+    component: TopMenu,
+    meta: {
+      title: 'Administration',
+      auth: false,
+      isAuth: false
+    },
+    children: [
+      {
+        path: 'roles',
+        name: 'admin.roles',
+        component: Roles,
+        meta: {
+          title: 'Wiki Roles',
+          auth: true,
+          isAuth: false
+        }
       }
     ]
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
+    meta: {
+      title: 'Login',
+      auth: false,
+      isAuth: true
+    }
   },
   {
     path: '/register',
     name: 'register',
-    component: Register
+    component: Register,
+    meta: {
+      title: 'Register',
+      auth: false,
+      isAuth: true
+    }
   },
   {
     path: '/error-page',
     name: 'error-page',
-    component: ErrorPage
+    component: ErrorPage,
+    meta: {
+      title: 'Error',
+      auth: false,
+      isAuth: false
+    }
   },
   {
     path: '/:pathMatch(.*)*',
-    component: ErrorPage
+    component: ErrorPage,
+    meta: {
+      title: 'Error',
+      auth: false,
+      isAuth: false
+    }
   }
 ]
 
@@ -56,6 +119,43 @@ const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     return savedPosition || { left: 0, top: 0 }
   }
+})
+
+router.beforeEach(async (to, from, next) => {
+  const toWithMeta = to.matched.slice().reverse().find(r => r.meta)
+  let isLoggedIn = false
+
+  console.log(localStorage.getItem('token'))
+
+  axios.defaults.headers.common['Content-Type'] = 'application/json'
+  axios.defaults.headers.common.Authorization = 'Bearer ' + localStorage.getItem('token')
+
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/auth/user')
+    isLoggedIn = response.data.success
+  } catch (error) { }
+
+  if (toWithMeta.meta?.auth && !isLoggedIn) {
+    next({
+      path: '/login',
+      query: {
+        redirect: toWithMeta.fullPath
+      }
+    })
+  }
+
+  if (toWithMeta.meta?.isAuth && isLoggedIn) {
+    next({
+      path: '/',
+      query: {
+        redirect: toWithMeta.fullPath
+      }
+    })
+  }
+
+  document.title = toWithMeta.meta.title || process.env.BASE_URL
+
+  next()
 })
 
 export default router
