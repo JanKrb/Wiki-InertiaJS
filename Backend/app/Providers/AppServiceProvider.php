0cbaaed76b252614a650be07ed73719c8f08f5cc
JanKrb <jankrb@jankrb.com>
2021-06-15 21:28:17 +0200

    Activity Log

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Badge;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use App\Observers\BadgeObserver;
+use App\Observers\PermissionObserver;
+use App\Observers\RoleObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -25,6 +33,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /**
+         * Observers for activities
+         */
+        User::observe(UserObserver::class);
+        Role::observe(RoleObserver::class);
+        Permission::observe(PermissionObserver::class);
+        Badge::observe(BadgeObserver::class);
+
         /**
          * Paginate a standard Laravel Collection.
          *
