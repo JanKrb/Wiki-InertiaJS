c1a3501bf4f0b2f30b16a5dc70c3e502863a3aed
JanKrb <jankrb@jankrb.com>
2021-06-20 12:16:10 +0200

    Empty Post Controllers

---
+++ Backend/app/Http/Controllers/API/Post/PostCommentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\Post;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class PostCommentController extends BaseController
+{
+    //
+}
