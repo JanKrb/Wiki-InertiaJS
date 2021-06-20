458956ad7f266b394695b89053ff50b5b58d9936
JanKrb <jankrb@jankrb.com>
2021-06-20 15:19:18 +0200

    Improve output and performance

--- Backend/app/Http/Resources/Post.php
+++ Backend/app/Http/Resources/Post.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Post extends JsonResource
 {
+    private $detailed;
+
+    public function __construct($resource, $detailed=false)
+    {
+        parent::__construct($resource);
+        $this->detailed = $detailed;
+    }
+
     /**
      * Transform the resource collection into an array.
      *
@@ -14,19 +22,32 @@ class Post extends JsonResource
      */
     public function toArray($request)
     {
-        return [
+        $data = [
             'id' => $this->id,
             'title' => $this->title,
             'content' => $this->content,
             'user' => $this->user,
-            'approved_by' => $this->approved_by,
             'tags' => new TagCollection($this->tags),
-            'histories' => new PostHistoryCollection($this->histories),
-            'votes' => new PostVoteCollection($this->votes),
-            'comments' => new PostCommentCollection($this->comments),
-            'approved_at' => $this->approved_at->format('Y-m-d h:m:i'),
             'created_at' => $this->created_at->format('Y-m-d h:m:i'),
             'updated_at' => $this->updated_at->format('Y-m-d h:m:i')
         ];
+
+        if ($this->approved_at) {
+            $data['approved_at'] = $this->approved_at->format('Y-m-d h:m:i');
+            $data['approved_by'] = $this->approved_user;
+        }
+
+        if ($this->detailed) {
+            $data['histories'] = new PostHistoryCollection($this->histories);
+            $data['votes'] = new PostVoteCollection($this->votes);
+            $data['comments'] = new PostCommentCollection($this->comments);
+        } else {
+            $data['histories'] = sizeof($this->histories);
+            $data['like_votes'] = sizeof($this->votes->where('vote', 1));
+            $data['dislike_votes'] = sizeof($this->votes->where('vote', 0));
+            $data['comments'] = sizeof($this->comments);
+        }
+
+        return $data;
     }
 }
