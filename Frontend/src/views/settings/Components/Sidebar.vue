732739265fc045e9804534b05ad286b325db2b9a
Nino Gralla <nino.gralla@gmx.de>
2021-06-22 01:56:12 +0200

    fix Settings sidebar & add new Subpages

--- Frontend/src/views/settings/Components/Sidebar.vue
+++ Frontend/src/views/settings/Components/Sidebar.vue
@@ -1,37 +1,53 @@
 <template>
     <!-- BEGIN: Inbox Menu -->
     <div class="intro-y box bg-theme-1 p-5 mt-6">
-        <button type="button" class="btn text-gray-700 dark:text-gray-300 w-full bg-white dark:bg-theme-1 mt-1">
-            TBS1 - ITA Wiki
-        </button>
-        <div class="border-t border-theme-3 dark:border-dark-5 mt-6 pt-6 text-white">
-          <a href="" class="flex items-center px-3 py-2 rounded-md bg-theme-20 dark:bg-dark-1 font-medium">
-              <GlobeIcon class="w-4 h-4 mr-2"/> General
+      <button type="button" class="btn text-gray-700 dark:text-gray-300 w-full bg-white dark:bg-theme-1 mt-1">
+        {{ this?.title }}
+      </button>
+      <div class="border-t border-theme-3 dark:border-dark-5 mt-6 pt-6 text-white">
+        <router-link :to="{ name: 'admin.settings.general' }">
+          <a :class="'flex items-center px-3 py-2 mt-2 rounded-md ' + (this.$route.name === 'admin.settings.general' ? 'bg-theme-20 dark:bg-dark-1 font-medium' : '')" href="">
+            <GlobeIcon class="w-4 h-4 mr-2"/> General
           </a>
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
-              <LockIcon class="w-4 h-4 mr-2"/> Security
-          </a>
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+        </router-link>
+        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+            <LockIcon class="w-4 h-4 mr-2"/> Security
+        </a>
+        <router-link :to="{ name: 'admin.settings.database' }">
+          <a :class="'flex items-center px-3 py-2 mt-2 rounded-md ' + (this.$route.name === 'admin.settings.database' ? 'bg-theme-20 dark:bg-dark-1 font-medium' : '')" href="">
               <DatabaseIcon class="w-4 h-4 mr-2"/> Database
           </a>
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
-              <UsersIcon class="w-4 h-4 mr-2"/> User
-          </a>
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+        </router-link>
+        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+            <UsersIcon class="w-4 h-4 mr-2"/> User
+        </a>
+        <router-link :to="{ name: 'admin.settings.mail' }">
+          <a :class="'flex items-center px-3 py-2 mt-2 rounded-md ' + (this.$route.name === 'admin.settings.mail' ? 'bg-theme-20 dark:bg-dark-1 font-medium' : '')" href="">
               <MailIcon class="w-4 h-4 mr-2"/> Mails
           </a>
-        </div>
-        <div class="border-t border-theme-3 dark:border-dark-5 mt-4 pt-4 text-white">
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
-            <LifeBuoyIcon class="w-4 h-4 mr-2"/>Support
-          </a>
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
-            <BookIcon class="w-4 h-4 mr-2"/>Documentation
-          </a>
-          <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
-            <SendIcon class="w-4 h-4 mr-2"/>Contact
-          </a>
-        </div>
+        </router-link>
+      </div>
+      <div class="border-t border-theme-3 dark:border-dark-5 mt-4 pt-4 text-white">
+        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+          <LifeBuoyIcon class="w-4 h-4 mr-2"/>Support
+        </a>
+        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+          <BookIcon class="w-4 h-4 mr-2"/>Documentation
+        </a>
+        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+          <SendIcon class="w-4 h-4 mr-2"/>Contact
+        </a>
+      </div>
     </div>
     <!-- END: Inbox Menu -->
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String
+  }
+})
+</script>
