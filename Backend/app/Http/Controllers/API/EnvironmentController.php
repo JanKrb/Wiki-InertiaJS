5c59fa523253dd072df7d2e4f94381061ed4a178
JanKrb <jankrb@jankrb.com>
2021-06-21 16:56:28 +0200

    Change email settings

--- Backend/app/Http/Controllers/API/EnvironmentController.php
+++ Backend/app/Http/Controllers/API/EnvironmentController.php
@@ -69,6 +69,56 @@ class EnvironmentController extends BaseController
         ], "Successfully changed mysql connection");
     }
 
+    public function update_mail(Request $request) {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'hostname' => 'required',
+            'port' => 'required|integer|min:1|max:99999',
+            'username' => 'required',
+            'password' => 'required',
+            'encryption' => 'required',
+            'from_address' => 'required',
+            'from_name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', ['errors' => $validator->errors()]);
+        }
+
+        // MAILER
+        config(['mail.mailers.smtp.host' => $input['hostname']]);
+        $this->putPermanentEnv('MAIL_HOST', $input['hostname']);
+
+        // PORT
+        config(['mail.mailers.smtp.port' => $input['port']]);
+        $this->putPermanentEnv('MAIL_PORT', $input['port']);
+
+        // USERNAME
+        config(['mail.mailers.smtp.username' => $input['username']]);
+        $this->putPermanentEnv('MAIL_USERNAME', $input['username']);
+
+        // PASSWORD
+        config(['mail.mailers.smtp.password' => $input['password']]);
+        $this->putPermanentEnv('MAIL_PASSWORD', $input['password']);
+
+        // ENCRYPTION
+        config(['mail.mailers.smtp.encryption' => $input['encryption']]);
+        $this->putPermanentEnv('MAIL_ENCRYPTION', $input['encryption']);
+
+        // FROM MAIL
+        config(['mail.from.address' => $input['from_address']]);
+        $this->putPermanentEnv('MAIL_FROM_ADDRESS', $input['from_address']);
+
+        // FROM NAME
+        config(['mail.from.name' => $input['from_name']]);
+        $this->putPermanentEnv('MAIL_FROM_NAME', $input['from_name']);
+
+        return $this->sendResponse([
+            "config" => config('mail'),
+        ], "Successfully changed mail settings");
+    }
+
     function putPermanentEnv($key, $value)
     {
         $path = app()->environmentFilePath();
