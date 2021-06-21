7835be7fd1422df974915042abdef2aa9ef99aca
Nino Gralla <nino.gralla@gmx.de>
2021-06-21 22:45:26 +0200

    remove Profile Emailsettings

--- Frontend/src/views/profile/Components/Sidebar.vue
+++ Frontend/src/views/profile/Components/Sidebar.vue
@@ -27,9 +27,6 @@
             <LockIcon class="w-4 h-4 mr-2"/> Change Password
           </a>
         </router-link>
-        <a class="flex items-center mt-3" href="">
-          <MailIcon class="w-4 h-4 mr-2"/> Email Settings
-        </a>
       </div>
       <div class="p-5 border-t border-gray-200 dark:border-dark-5">
         <a class="flex items-center" href="">
