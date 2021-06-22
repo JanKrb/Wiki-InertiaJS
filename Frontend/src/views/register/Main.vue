aeaaece422fb3201d253c937075cae050e2186a6
JanKrb <jankrb@jankrb.com>
2021-06-22 18:26:47 +0200

    Fix login link & design

--- Frontend/src/views/register/Main.vue
+++ Frontend/src/views/register/Main.vue
@@ -37,10 +37,10 @@
         <!-- END: Register Info -->
         <!-- BEGIN: Register Form -->
         <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
-          <form @submit.prevent="handleSubmit">
-            <div
-              class="my-auto mx-auto xl:ml-20 bg-white dark:bg-dark-1 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto"
-            >
+          <div
+            class="my-auto mx-auto xl:ml-20 bg-white dark:bg-dark-1 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto"
+          >
+            <form @submit.prevent="handleSubmit">
               <h2
                 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left"
               >
@@ -119,14 +119,14 @@
                 >
                   Register
                 </button>
-                <button
+                <router-link :to="{ name: 'login' }"
                   class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top"
                 >
                   Sign in
-                </button>
+                </router-link>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
         <!-- END: Register Form -->
       </div>
