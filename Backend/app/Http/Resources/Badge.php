e97507b46561ee6c8acc65ed8d0f51d05d21b755
JanKrb <jankrb@jankrb.com>
2021-06-22 17:22:22 +0200

    Fix typo

--- Backend/app/Http/Resources/Badge.php
+++ Backend/app/Http/Resources/Badge.php
@@ -24,7 +24,7 @@ class Badge extends JsonResource
             'icon' => $this->icon,
             'color' => $this->color,
             'is_role_badge' => $this->is_role_badge,
-            'role' => $this->role_id != null ? new RoleResource($this->role_id) : null,
+            'role' => $this->role_id != null ? new RoleResource($this->role) : null,
             'user' => new UserResource($this->user),
             'created_at' => $this->created_at->format('Y-m-d h:m:i'),
             'updated_at' => $this->updated_at->format('Y-m-d h:m:i')
