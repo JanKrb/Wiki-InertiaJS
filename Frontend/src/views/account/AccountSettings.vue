fcf3a8bb4de37828f6b752f0ef24094c165adab7
Nino Gralla <nino.gralla@gmx.de>
2021-06-23 23:15:14 +0200

    correct wrong Title

--- Frontend/src/views/account/AccountSettings.vue
+++ Frontend/src/views/account/AccountSettings.vue
@@ -12,7 +12,7 @@
         <form @submit.prevent="submitCredentials(this.user)">
           <div class="intro-y box lg:mt-5">
             <div class="flex items-center p-3 border-b border-gray-200 dark:border-dark-5">
-              <h2 class="font-medium text-base mr-auto">Roles & Tags</h2>
+              <h2 class="font-medium text-base mr-auto">Roles & Badges</h2>
               <button class="btn btn-primary btn-sm ml-auto" type="submit"><SaveIcon class="mr-2 w-5 h-5"></SaveIcon>Save</button>
             </div>
             <div class="p-5">
