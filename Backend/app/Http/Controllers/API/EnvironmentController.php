6125a1197909e5739151ac5c7f6cdbfda7b568a9
JanKrb <jankrb@jankrb.com>
2021-06-25 20:27:13 +0200

    Remove unused imported lib

--- Backend/app/Http/Controllers/API/EnvironmentController.php
+++ Backend/app/Http/Controllers/API/EnvironmentController.php
@@ -7,7 +7,6 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use mysqli;
-use Swift_SmtpTransport;
 
 class EnvironmentController extends BaseController
 {
