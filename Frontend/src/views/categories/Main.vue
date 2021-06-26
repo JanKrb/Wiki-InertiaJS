0876cf35706492ac9105851c474b153af240d524
Nino Gralla <nino.gralla@gmx.de>
2021-06-27 01:21:54 +0200

    add Post layout & fix Routing

--- Frontend/src/views/categories/Main.vue
+++ Frontend/src/views/categories/Main.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-12 gap-5">
     <div class="col-span-12 xxl:col-span-10">
       <div class="grid grid-cols-12 gap-5 mt-6 -mb-6">
-        <!-- BEGIN: Blog Layout -->
+        <!-- BEGIN: Categories Layout -->
         <div
           v-for="category in this.categories"
           v-bind:key="category.id"
@@ -57,21 +57,81 @@
               <div class="mr-3">
                 <FolderIcon class="mr-1 w-5 h-5"></FolderIcon><span class="font-medium">Category</span>
               </div>
+            </div>
+          </div>
+        </div>
+        <!-- END: Categories Layout -->
+
+        <!-- BEGIN: Posts Layout -->
+        <div
+          v-for="post in this.posts"
+          v-bind:key="post.id"
+          class="intro-y blog col-span-12 md:col-span-4 box"
+        >
+          <div class="blog__preview image-fit">
+            <img
+              :alt="'Thumbnail of ' + post.title"
+              class="rounded-t-md"
+              src=""
+            />
+            <div class="absolute w-full flex items-center px-5 pt-6 z-10">
+              <div class="w-10 h-10 flex-none image-fit">
+                <img
+                  :alt="'Thumbnail of ' + post.title"
+                  class="rounded-full"
+                  :src="post.user?.profile_picture"
+                />
+              </div>
+              <div class="ml-3 text-white mr-auto">
+                <a href="" class="font-medium">{{ post.user?.name }}</a>
+                <div class="text-xs mt-0.5">{{ post.updated_at }}</div>
+              </div>
+              <div class="dropdown ml-3" v-if='this.permissions?.categories_update'>
+                <a
+                  href="javascript:;"
+                  class="blog__action dropdown-toggle w-8 h-8 flex items-center justify-center rounded-full"
+                  aria-expanded="false"
+                >
+                  <MoreVerticalIcon class="w-4 h-4 text-white"/>
+                </a>
+                <div class="dropdown-menu w-40">
+                  <div class="dropdown-menu__content box dark:bg-dark-1 p-2">
+                    <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">
+                      <Edit2Icon class="w-4 h-4 mr-2"/> Edit
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="absolute bottom-0 text-white px-5 pb-6 z-10">
+              <a href="javascript:;" class="block font-medium text-xl mt-3" @click="this.showSubcategories(post)">
+                {{ post.title }}
+              </a>
+            </div>
+          </div>
+          <div class="p-5 text-gray-700 dark:text-gray-600">
+            {{ post.content.substring(0,200)+"..." }}
+          </div>
+          <div class="px-5 pt-3 pb-5 border-t border-gray-200 dark:border-dark-5">
+            <div class="w-full flex text-gray-600 text-xs sm:text-sm">
+              <div class="mr-3">
+                <FileTextIcon class="mr-1 w-5 h-5"></FileTextIcon><span class="font-medium">Post</span>
+              </div>
               <div class="ml-auto">
-                <HeartIcon class="mr-1 ml-4 w-5 h-5"></HeartIcon><span class="font-medium">1</span>
-                <EyeIcon class="mr-1 ml-4 w-5 h-5"></EyeIcon><span class="font-medium">1k</span>
-                <BookmarkIcon class="mr-1 ml-4 w-5 h-5"></BookmarkIcon><span class="font-medium">3k</span>
+                <HeartIcon class="mr-1 ml-4 w-5 h-5"></HeartIcon><span class="font-medium">{{ post.votes }}</span>
+                <MessageSquareIcon class="mr-1 ml-4 w-5 h-5"></MessageSquareIcon><span class="font-medium">{{ post.comments }}</span>
+                <BookmarkIcon class="mr-1 ml-4 w-5 h-5"></BookmarkIcon>
               </div>
             </div>
           </div>
         </div>
-        <!-- END: Blog Layout -->
+        <!-- END: Posts Layout -->
       </div>
     </div>
     <div class="col-span-12 xxl:col-span-2">
       <div class="xxl:border-l border-theme-5 -mb-10 pb-10">
         <div class="xxl:pl-6 grid grid-cols-12 gap-5">
-          <div class="col-span-12 md:col-span-6 xl:col-span-12 xl:col-start-1 xl:row-start-1 xxl:col-start-auto xxl:row-start-auto mt-3" v-if="this.selectedSubcategory !== 0">
+          <div class="col-span-12 md:col-span-6 xl:col-span-12 xl:col-start-1 xl:row-start-1 xxl:col-start-auto xxl:row-start-auto mt-3" v-if="this.$route.name === 'categories.subcategory'">
             <div class="mt-5 intro-x">
               <router-link :to="{ name: 'categories' }">
                 <button class="btn btn-primary shadow-md mr-2">Back to Dashboard</button>
@@ -137,9 +197,7 @@
                 class="intro-x"
               >
                 <div class="box px-5 py-3 mb-3 flex items-center zoom-in">
-                  <div
-                    class="w-10 h-10 flex-none image-fit rounded-full overflow-hidden"
-                  >
+                  <div class="w-10 h-10 flex-none image-fit rounded-full overflow-hidden">
                     <img
                       alt=""
                       :src="activity?.user?.profile_picture"
@@ -179,14 +237,28 @@ export default defineComponent({
       categories: [],
       announcementsLoading: true,
       permissions: {},
-      selectedSubcategory: 0
+      posts: []
     }
   },
   mounted() {
-    this.loadCategories()
     this.loadAnnouncements()
     this.loadRecent()
     this.testPagePermissions()
+
+    if (this.$route.name === 'categories.subcategory') {
+      this.loadSubcategory(this.$route.params.id)
+    } else {
+      this.loadCategories()
+    }
+  },
+  watch: {
+    $route(to, from) {
+      if (this.$route.name === 'categories.subcategory') {
+        this.loadSubcategory(this.$route.params.id)
+      } else {
+        this.loadCategories()
+      }
+    }
   },
   methods: {
     loadRecent() {
@@ -209,7 +281,6 @@ export default defineComponent({
         })
     },
     loadCategories() {
-      this.selectedSubcategory = 0
       axios.get('http://localhost:8000/api/categories/structured')
         .then((res) => {
           this.categories = res.data.data
@@ -220,13 +291,19 @@ export default defineComponent({
     },
     showSubcategories(category) {
       if (category.children.length > 0) {
-        this.loadPosts(category.id)
-        this.categories = category.children
-        this.selectedSubcategory = category.id
+        this.$router.push({ name: 'categories.subcategory', params: { id: category.id } })
       }
     },
-    loadPosts(id) {
-      axios.get()
+    loadSubcategory(id) {
+      axios.get('http://localhost:8000/api/posts')
+        .then(response => {
+          this.posts = response.data.data
+        })
+
+      axios.get('http://localhost:8000/api/categories/' + id)
+        .then(response => {
+          this.categories = response.data.data.children
+        })
     },
     testPagePermissions() {
       axios.post('http://localhost:8000/api/permissions/test', {
