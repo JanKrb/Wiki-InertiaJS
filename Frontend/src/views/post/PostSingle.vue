3ab3d096cb6b7adb327ce11d527b2f2a6b3ede35
Nino Gralla <nino.gralla@gmx.de>
2021-07-01 23:12:28 +0200

    add Single Post view

--- Frontend/src/views/post/PostSingle.vue
+++ Frontend/src/views/post/PostSingle.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    <div class="intro-y news xl:w-3/5 p-5 box mt-8">
+    <div class="intro-y news xl:w-4/5 p-10 box mt-8">
       <!-- BEGIN: Blog Layout -->
       <h2 class="intro-y font-medium text-xl sm:text-2xl">
-        {{ $f()[0].news[0].title }}
+        {{ this.post?.title }}
       </h2>
-      <div
-        class="intro-y text-gray-700 dark:text-gray-600 mt-3 text-xs sm:text-sm"
-      >
-        {{ $f()[0].dates[0] }} <span class="mx-1">•</span>
-        <a class="text-theme-1 dark:text-theme-10" href="">{{
-            $f()[0].products[0].category
-          }}</a>
-        <span class="mx-1">•</span> 7 Min read
+      <div class="intro-y text-gray-700 dark:text-gray-600 mt-3 text-xs sm:text-sm">
+        {{ this.post?.created_at }} <span class="mx-1">•</span>
+        <a class="text-theme-1 dark:text-theme-10" href="">
+            Parent Category
+        </a>
+        <span class="mx-1">•</span> {{ this.post?.content.split(' ').length / 1000 * 60 }} Min read
       </div>
       <div class="intro-y mt-6">
         <div class="news__preview image-fit">
           <img
-            alt="Icewall Tailwind HTML Admin Template"
+            alt=""
             class="rounded-md"
-            :src="require(`@/assets/images/${$f()[0].images[0]}`)"
+            src="https://images.pexels.com/photos/2469122/pexels-photo-2469122.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
           />
         </div>
       </div>
@@ -36,66 +34,41 @@
           <div class="intro-x w-8 h-8 sm:w-10 sm:h-10 image-fit">
             <Tippy
               tag="img"
-              alt="Icewall Tailwind HTML Admin Template"
+              alt=""
               class="rounded-full border border-white zoom-in"
-              :src="require(`@/assets/images/${$f()[0].photos[0]}`)"
+              src="https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
               :content="$f()[0].users[0].name"
             />
           </div>
           <div class="intro-x w-8 h-8 sm:w-10 sm:h-10 image-fit -ml-4">
             <Tippy
               tag="img"
-              alt="Icewall Tailwind HTML Admin Template"
+              alt=""
               class="rounded-full border border-white zoom-in"
-              :src="require(`@/assets/images/${$f()[0].photos[1]}`)"
+              src="https://images.pexels.com/photos/7410003/pexels-photo-7410003.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
               :content="$f()[0].users[1].name"
             />
           </div>
           <div class="intro-x w-8 h-8 sm:w-10 sm:h-10 image-fit -ml-4">
             <Tippy
               tag="img"
-              alt="Icewall Tailwind HTML Admin Template"
+              alt=""
               class="rounded-full border border-white zoom-in"
-              :src="require(`@/assets/images/${$f()[0].photos[2]}`)"
+              src="https://images.pexels.com/photos/8412730/pexels-photo-8412730.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
               :content="$f()[0].users[2].name"
             />
           </div>
         </div>
-        <div
-          class="absolute sm:relative -mt-12 sm:mt-0 w-full flex text-gray-700 dark:text-gray-600 text-xs sm:text-sm"
-        >
-          <div class="intro-x mr-1 sm:mr-3">
-            Comments:
-            <span class="font-medium">{{ $f()[0].totals[0] }}</span>
-          </div>
-          <div class="intro-x mr-1 sm:mr-3">
-            Views: <span class="font-medium">{{ $f()[0].totals[1] }}k</span>
-          </div>
+        <div class="absolute sm:relative -mt-12 sm:mt-0 w-full flex text-gray-700 dark:text-gray-600 text-xs sm:text-sm">
           <div class="intro-x sm:mr-3 ml-auto">
-            Likes: <span class="font-medium">{{ $f()[0].totals[2] }}k</span>
+            Comments: <span class="font-medium mr-4">{{ this.post?.comments }}</span>
+            Changes: <span class="font-medium mr-4">{{ this.post?.histories }}</span>
+            Likes: <span class="font-medium">{{ this.post?.like_votes }}</span>
           </div>
         </div>
-        <Tippy
-          tag="a"
-          href=""
-          class="intro-x w-8 h-8 sm:w-10 sm:h-10 flex flex-none items-center justify-center rounded-full bg-theme-14 dark:bg-dark-5 dark:text-gray-300 text-theme-10 ml-auto sm:ml-0"
-          content="Share"
-        >
-          <Share2Icon class="w-3 h-3" />
-        </Tippy>
-        <Tippy
-          tag="a"
-          href=""
-          class="intro-x w-8 h-8 sm:w-10 sm:h-10 flex flex-none items-center justify-center rounded-full bg-theme-1 text-white ml-3"
-          content="Download PDF"
-        >
-          <ShareIcon class="w-3 h-3" />
-        </Tippy>
       </div>
       <div class="intro-y text-justify leading-relaxed">
-        <p class="mb-5">{{ $f()[1].news[0].content }}</p>
-        <p class="mb-5">{{ $f()[2].news[0].content }}</p>
-        <p>{{ $f()[3].news[0].content }}</p>
+        <p>{{ this.post?.content }}</p>
       </div>
       <div
         class="intro-y flex text-xs sm:text-sm flex-col sm:flex-row items-center mt-5 pt-5 border-t border-gray-200 dark:border-dark-5"
@@ -103,105 +76,73 @@
         <div class="flex items-center">
           <div class="w-12 h-12 flex-none image-fit">
             <img
-              alt="Icewall Tailwind HTML Admin Template"
+              alt=""
               class="rounded-full"
-              :src="require(`@/assets/images/${$f()[0].photos[0]}`)"
+              :src="this.post?.user?.profile_picture"
             />
           </div>
           <div class="ml-3 mr-auto">
-            <a href="" class="font-medium">{{ $f()[0].users[0].name }}</a
-            >, Author
-            <div class="text-gray-600">Senior Frontend Engineer</div>
+            <a href="" class="font-medium">
+              {{ this.post?.user?.name }}
+            </a>, Author
+            <div class="text-gray-600">{{ this.post?.user?.email }}</div>
           </div>
         </div>
         <div
           class="flex items-center text-gray-700 dark:text-gray-600 sm:ml-auto mt-5 sm:mt-0"
         >
-          Share this post:
+          Rate this Post:
           <Tippy
             tag="a"
             href=""
             class="w-8 h-8 sm:w-10 sm:h-10 rounded-full flex items-center justify-center border dark:border-dark-5 ml-2 text-gray-500 zoom-in"
-            content="Facebook"
+            content="Like"
           >
-            <FacebookIcon class="w-3 h-3 fill-current" />
+            <ThumbsUpIcon class="w-3 h-3 fill-current" />
           </Tippy>
           <Tippy
             tag="a"
             href=""
             class="w-8 h-8 sm:w-10 sm:h-10 rounded-full flex items-center justify-center border dark:border-dark-5 ml-2 text-gray-500 zoom-in"
-            content="Twitter"
+            content="Dislike"
           >
-            <TwitterIcon class="w-3 h-3 fill-current" />
-          </Tippy>
-          <Tippy
-            tag="a"
-            href=""
-            class="w-8 h-8 sm:w-10 sm:h-10 rounded-full flex items-center justify-center border dark:border-dark-5 ml-2 text-gray-500 zoom-in"
-            content="Linked In"
-          >
-            <LinkedinIcon class="w-3 h-3 fill-current" />
+            <ThumbsDownIcon class="w-3 h-3 fill-current" />
           </Tippy>
         </div>
       </div>
       <!-- END: Blog Layout -->
       <!-- BEGIN: Comments -->
-      <div
-        class="intro-y mt-5 pt-5 border-t border-gray-200 dark:border-dark-5"
-      >
-        <div class="text-base sm:text-lg font-medium">2 Responses</div>
+      <div class="intro-y my-5 pt-5 border-t border-gray-200 dark:border-dark-5">
+        <div class="text-base sm:text-lg font-medium">{{ this.post?.comments }} Responses</div>
         <div class="news__input relative mt-5">
           <MessageCircleIcon
             class="w-5 h-5 absolute my-auto inset-y-0 ml-6 left-0 text-gray-600"
           />
           <textarea
-            class="form-control border-transparent bg-gray-200 pl-16 py-6 placeholder-theme-13 resize-none"
+            class="form-control border-transparent bg-gray-300 pl-16 py-6 placeholder-theme-13 resize-none"
             rows="1"
             placeholder="Post a comment..."
           ></textarea>
         </div>
       </div>
-      <div class="intro-y mt-5 pb-10">
-        <div class="pt-5">
-          <div class="flex">
-            <div class="w-10 h-10 sm:w-12 sm:h-12 flex-none image-fit">
-              <img
-                alt="Icewall Tailwind HTML Admin Template"
-                class="rounded-full"
-                :src="require(`@/assets/images/${$f()[0].photos[0]}`)"
-              />
-            </div>
-            <div class="ml-3 flex-1">
-              <div class="flex items-center">
-                <a href="" class="font-medium">{{ $f()[0].users[0].name }}</a>
-                <a href="" class="ml-auto text-xs text-gray-600">Reply</a>
-              </div>
-              <div class="text-gray-600 text-xs sm:text-sm">
-                {{ $f()[0].formattedTimes[0] }}
-              </div>
-              <div class="mt-2">{{ $f()[0].news[0].shortContent }}</div>
-            </div>
+      <div class="pb-3" v-for="comment in this.post.post_comments" v-bind:key="comment.id">
+        <div class="flex box p-3 bg-gray-200">
+          <div class="w-10 h-10 sm:w-12 sm:h-12 flex-none image-fit">
+            <img
+              alt=""
+              class="rounded-full"
+              :src="comment?.user?.profile_picture"
+            />
           </div>
-        </div>
-        <div class="mt-5 pt-5 border-t border-gray-200 dark:border-dark-5">
-          <div class="flex">
-            <div class="w-10 h-10 sm:w-12 sm:h-12 flex-none image-fit">
-              <img
-                alt="Icewall Tailwind HTML Admin Template"
-                class="rounded-full"
-                :src="require(`@/assets/images/${$f()[0].photos[1]}`)"
-              />
+          <div class="ml-3 flex-1">
+            <div class="flex items-center">
+              <a href="" class="font-medium">{{ comment?.user?.name }}</a>
+              <a href="" class="ml-auto text-xs text-gray-600">Reply</a>
             </div>
-            <div class="ml-3 flex-1">
-              <div class="flex items-center">
-                <a href="" class="font-medium">{{ $f()[0].users[1].name }}</a>
-                <a href="" class="ml-auto text-xs text-gray-600">Reply</a>
-              </div>
-              <div class="text-gray-600 text-xs sm:text-sm">
-                {{ $f()[1].formattedTimes[0] }}
-              </div>
-              <div class="mt-2">{{ $f()[1].news[0].shortContent }}</div>
+            <div class="text-gray-600 text-xs sm:text-sm">
+              {{ comment?.created_at }}
             </div>
+            <div class="mt-2">{{ comment?.content }}</div>
           </div>
         </div>
       </div>
@@ -217,7 +158,11 @@ import axios from 'axios'
 export default defineComponent({
   data() {
     return {
-      post: {}
+      post: {
+        title: '',
+        content: '',
+        post_comments: []
+      }
     }
   },
   mounted() {
@@ -228,6 +173,17 @@ export default defineComponent({
       axios.get('http://localhost:8000/api/posts/' + id)
         .then(response => {
           this.post = response.data.data
+          console.log(response)
+          this.loadComments(id)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+    loadComments(id) {
+      axios.get('http://localhost:8000/api/posts/' + id + '/comments')
+        .then(response => {
+          this.post.post_comments = response.data.data
           console.log(response)
         })
         .catch(error => {
