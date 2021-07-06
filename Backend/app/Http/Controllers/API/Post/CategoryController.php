c4531b0aca911a440a5486cbf7e1fd80fc86cfc1
JanKrb <jankrb@jankrb.com>
2021-07-06 22:47:41 +0200

    Remove childs from get all cats

--- Backend/app/Http/Controllers/API/Post/CategoryController.php
+++ Backend/app/Http/Controllers/API/Post/CategoryController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\API\Post;
 use App\Http\Controllers\BaseController;
 use App\Http\Resources\CategoryCollection;
 use App\Http\Resources\Category as CategoryResource;
+use App\Http\Resources\SmallCategoryCollection;
 use App\Http\Resources\StructuredCategory as StructuredCategoryResource;
 use App\Models\Category;
 use App\Http\Resources\StructuredCategoryCollection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends BaseController
 {
@@ -36,5 +38,73 @@ class CategoryController extends BaseController
             'success' => true,
             'message' => 'Successfully retrieved categories'
         ]);
+    }
+
+    /**
+     * Get all data
+     *
+     * @param Request $request
+     */
+    public function get_all(Request $request)
+    {
+        /**
+         * Sort Indices
+         *
+         * 0 - SORT_REGULAR
+         * 1 - SORT_STRING
+         * 3 - SORT_DESC
+         * 4 - SORT_ASC
+         * 5 - SORT_LOCALE_STRING
+         * 6 - SORT_NATURAL
+         * 8 - SORT_FLAG_CASE
+         */
+        $validator = Validator::make($request->all(), [
+            'per_page' => 'integer',
+            'paginate' => 'boolean',
+            'sort' => 'array',
+            'sort.column' => 'string|required_with:sort',
+            'sort.method' => 'integer|required_with:sort',
+            'additional' => 'array',
+            'recent' => 'integer'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', ['errors' => $validator->errors()], 400);
+        }
+
+        $data = $this->model::all();
+
+        $per_page = $request->get('per_page', 15);
+        $paginate_data = $request->get('paginate', true);
+        $recent = $request->get('recent', 0);
+
+        if ($recent > 0) {
+            $data = $data->sortBy('updated_at', SORT_ASC)->take($recent);
+        }
+
+        if ($request->has('sort')) {
+            $data = $data->sortBy(
+                $request->get('sort.column', 'id'),
+                $request->get('sort.method', SORT_ASC)
+            );
+        }
+
+        if ($paginate_data) {
+            $data = $data->paginate($per_page);
+        }
+
+        $response = (new SmallCategoryCollection($data));
+
+        if ($request->has('additional')) {
+            $additional = $request->get('additional');
+
+            $response = $response::additional(array_merge([
+                'success' => true,
+                'message' => 'Successfully retrieved announcements'
+            ],
+                $additional));
+        }
+
+        return $response;
     }
 }
