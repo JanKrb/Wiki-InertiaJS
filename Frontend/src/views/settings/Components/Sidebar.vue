49ecc21990bbc69e432a912179f7269135dc6c50
Nino Gralla <nino.gralla@gmx.de>
2021-07-21 02:39:46 +0200

    Add new Links to Settings sidebar

--- Frontend/src/views/settings/Components/Sidebar.vue
+++ Frontend/src/views/settings/Components/Sidebar.vue
@@ -27,13 +27,13 @@
         </router-link>
       </div>
       <div class="border-t border-theme-3 dark:border-dark-5 mt-4 pt-4 text-white">
-        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+        <a :href="this.supportlink" class="flex items-center px-3 py-2 mt-2 rounded-md">
           <LifeBuoyIcon class="w-4 h-4 mr-2"/>Support
         </a>
-        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+        <a :href="this.docslink" class="flex items-center px-3 py-2 mt-2 rounded-md">
           <BookIcon class="w-4 h-4 mr-2"/>Documentation
         </a>
-        <a href="" class="flex items-center px-3 py-2 mt-2 rounded-md">
+        <a :href="this.contactlink" class="flex items-center px-3 py-2 mt-2 rounded-md">
           <SendIcon class="w-4 h-4 mr-2"/>Contact
         </a>
       </div>
@@ -47,6 +47,13 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   props: {
     title: String
+  },
+  data() {
+    return {
+      supportlink: process.env.VUE_APP_SUPPORTLINK,
+      docslink: process.env.VUE_APP_DOCUMENTATIONLINK,
+      contactlink: process.env.VUE_APP_CONTACTLINK
+    }
   }
 })
 </script>
