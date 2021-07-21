27044b340cc06261905a655f669594b14cbe14b6
Nino Gralla <nino.gralla@gmx.de>
2021-07-21 02:39:26 +0200

    Change default Logo Add viewable Settings Edit Info message

--- Frontend/src/views/settings/SettingsGeneral.vue
+++ Frontend/src/views/settings/SettingsGeneral.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-12 gap-6 mt-8">
     <div class="col-span-12 lg:col-span-3 xxl:col-span-2">
       <h2 class="intro-y text-lg font-medium mr-auto mt-2">Wiki Settings</h2>
-      <Sidebar :title="this.details.name"></Sidebar>
+      <Sidebar :title="this.wiki_settings.name"></Sidebar>
     </div>
     <div class="col-span-12 lg:col-span-9 xxl:col-span-10">
       <div class="intro-y text-right items-center">
@@ -10,7 +10,7 @@
           tag="a"
           href="javascript:;"
           class="tooltip btn px-2 box text-gray-700 dark:text-gray-300"
-          content="These are the general wiki settings, here you can set your wiki data!"
+          content="These are the general wiki settings, here you can view your wiki data!"
         >
           <span class="flex items-center justify-center">
             <InfoIcon class="w-5 h-5 mr-2"/>Informations
@@ -24,148 +24,120 @@
         </div>
         <div class="overflow-x-auto sm:overflow-x-visible">
           <div class="intro-y p-5">
-            <form @submit.prevent="saveSettings">
-              <div class="flex flex-col-reverse xl:flex-row flex-col">
-                <div class="flex-1 mt-6 xl:mt-0">
-                  <div class="grid grid-cols-12 gap-x-5">
-                    <div class="col-span-12 xxl:col-span-12">
-                      <div>
-                        <label for="update-profile-form-1" class="form-label">
-                          Name
-                        </label>
-                        <input
-                          id="update-profile-form-1"
-                          type="text"
-                          class="form-control"
-                          placeholder="Enter Wiki-Name"
-                          v-model='settings.name'
-                        />
-                      </div>
-                      <div class="mt-3 xxl:mt-5">
-                        <label for="accounts-email-edit" class="form-label">
-                          Description
-                        </label>
-                        <textarea
-                          id="accounts-email-edit"
-                          type="text"
-                          class="form-control"
-                          placeholder="Enter Wiki-Description"
-                          v-model='settings.description'
-                        />
-                      </div>
-                    </div>
-                    <div class="col-span-12">
-                      <hr class="mt-5 mb-3">
-                    </div>
-                    <div class="col-span-12 xxl:col-span-6">
-                      <div class="mt-3 xxl:mt-0">
-                        <label for="accounts-email_verify-edit" class="form-label">
-                          Placeholder
-                        </label>
-                        <input
-                          id="accounts-email_verify-edit"
-                          type="text"
-                          class="form-control"
-                          placeholder="Input Placeholder"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-span-12 xxl:col-span-6">
-                      <div class="mt-3 xxl:mt-0">
-                        <label for="accounts-id-edit" class="form-label">
-                          Placeholder
-                        </label>
-                        <input
-                          id="accounts-id-edit"
-                          type="text"
-                          class="form-control"
-                          placeholder="Input Placeholder"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="flex justify-end mt-4">
-                    <button type="submit" class="btn btn-primary w-20 mr-auto">
-                      Save
-                    </button>
-                  </div>
-                </div>
-                <div class="w-52 mx-auto xl:mr-0 xl:ml-6">
-                  <div class="border-2 border-dashed shadow-sm border-gray-200 dark:border-dark-5 rounded-md p-5">
-                    <div class="h-40 relative image-fit cursor-pointer zoom-in mx-auto">
-                      <img class="rounded-md" alt="" :src="this.details.logo"/>
-                      <Tippy
-                        tag="div"
-                        content="Remove this logo?"
-                        class="w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-theme-6 right-0 top-0 -mr-2 -mt-2"
-                      >
-                        <xIcon class="w-4 h-4"/>
-                      </Tippy>
-                    </div>
-                    <div class="mx-auto cursor-pointer relative mt-5">
-                      <button type="button" class="btn btn-primary w-full">
-                        Change Logo
-                      </button>
+            <div class="flex flex-col-reverse xl:flex-row flex-col">
+              <div class="flex-1 mt-6 xl:mt-0">
+                <div class="grid grid-cols-12 gap-x-5">
+                  <div class="col-span-12 xxl:col-span-12">
+                    <div>
+                      <label for="view-settings-name" class="form-label">
+                        Name
+                      </label>
                       <input
-                        type="file"
-                        class="w-full h-full top-0 left-0 absolute opacity-0"
+                        id="view-settings-name"
+                        type="text"
+                        class="form-control"
+                        placeholder="No wiki name was given"
+                        v-model='wiki_settings.name'
+                        disabled
+                      />
+                    </div>
+                    <div class="mt-3 xxl:mt-5">
+                      <label for="view-settings-description" class="form-label">
+                        Description
+                      </label>
+                      <textarea
+                        id="view-settings-description"
+                        type="text"
+                        class="form-control"
+                        placeholder="No wiki description was given"
+                        v-model='wiki_settings.description'
+                        disabled
+                      />
+                    </div>
+                    <div>
+                      <label for="view-settings-domain" class="form-label mt-3 xxl:mt-5">
+                        Domain
+                      </label>
+                      <input
+                        id="view-settings-domain"
+                        type="url"
+                        class="form-control"
+                        placeholder="No wiki domain was given"
+                        v-model='wiki_settings.domain'
+                        disabled
                       />
                     </div>
                   </div>
                 </div>
               </div>
-            </form>
-          </div>
-        </div>
-        <div class="p-3 flex flex-col sm:flex-row items-center text-center sm:text-left text-gray-600">
-          <div class="sm:ml-auto mt-5 sm:mt-0 dark:text-gray-300">
-            Last update: 36 minutes ago
-          </div>
-        </div>
-      </div>
-      <!-- END: Inbox Content -->
-      <div class="intro-y inbox box mt-5">
-        <div class="p-5 flex flex-col-reverse sm:flex-row text-gray-600 border-b border-gray-200 dark:border-dark-1">
-          <h2 class="font-medium text-base mr-auto">Settings</h2>
-        </div>
-        <div class="overflow-x-auto sm:overflow-x-visible">
-          <div class="intro-y p-5">
-            <div class="grid grid-cols-12 gap-x-5">
-              <div class="col-span-6">
-                <div class="flex items-center">
-                  <div class="border-l-2 border-theme-1 pl-4">
-                    <a href="" class="font-medium">
-                      Allow Darkmode
-                    </a>
-                    <div class="text-gray-600">
-                      This setting allows you to Disable a Darkmode for the Wiki
-                    </div>
+              <div class="w-52 mx-auto xl:mr-0 xl:ml-6">
+                <label class="form-label">
+                  Logo
+                </label>
+                <div class="border-2 shadow-sm border-gray-200 dark:border-dark-5 rounded-md p-5">
+                  <div class="h-40 mx-auto image-fit">
+                    <img alt="" :src="this.wiki_settings.logo ? this.wiki_settings.logo : require('@/assets/images/placeholder.png')"/>
                   </div>
-                  <input class="form-check-switch ml-auto" type="checkbox">
-                </div>
-              </div>
-              <div class="col-span-6">
-                <div class="flex items-center">
-                  <div class="border-l-2 border-theme-1 pl-4">
-                    <a href="" class="font-medium">
-                      Allow Anonym usage
-                    </a>
-                    <div class="text-gray-600">
-                      This setting allows you to force a login for all Users
-                    </div>
-                  </div>
-                  <input class="form-check-switch ml-auto" type="checkbox">
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="p-3 flex flex-col sm:flex-row items-center text-center sm:text-left text-gray-600">
-          <div class="sm:ml-auto mt-5 sm:mt-0 dark:text-gray-300">
-            Last update: 36 minutes ago
+      </div>
+      <div class="intro-y inbox box mt-5">
+        <div class="overflow-x-auto sm:overflow-x-visible">
+          <div class="intro-y p-5">
+            <div class="flex flex-col-reverse xl:flex-row flex-col">
+              <div class="flex-1 xl:mt-0">
+                <div class="grid grid-cols-12 gap-x-5">
+                  <div class="col-span-12 xxl:col-span-12">
+                    <div>
+                      <label for="view-settings-contactmail" class="form-label">
+                        Contact Mail
+                      </label>
+                      <input
+                        id="view-settings-contactmail"
+                        type="url"
+                        class="form-control"
+                        placeholder="No wiki contact mail was given"
+                        v-model='wiki_settings.contactmail'
+                        disabled
+                      />
+                    </div>
+                    <div>
+                      <label for="view-settings-pagetitle" class="form-label mt-3 xxl:mt-5">
+                        Page Title
+                      </label>
+                      <input
+                        id="view-settings-pagetitle"
+                        type="url"
+                        class="form-control"
+                        placeholder="No wiki page title was given"
+                        :value="wiki_settings.pagetitle ? wiki_settings.pagetitle + ' {PAGETITLE}' : null"
+                        disabled
+                      />
+                    </div>
+                    <div>
+                      <label for="view-settings-supportlink" class="form-label mt-3 xxl:mt-5">
+                        Support Link
+                      </label>
+                      <input
+                        id="view-settings-supportlink"
+                        type="url"
+                        class="form-control"
+                        placeholder="No wiki support link was given"
+                        :value="wiki_settings.supportlink"
+                        disabled
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <!-- END: Inbox Content -->
     </div>
   </div>
 </template>
@@ -180,25 +152,15 @@ export default defineComponent({
   },
   data() {
     return {
-      details: {
-        name: this.$config.name,
-        logo: this.$config.logo,
-        description: this.$config.description
-      },
-      settings: {
-        name: this.$config.name,
-        description: this.$config.description
+      wiki_settings: {
+        name: process.env.VUE_APP_NAME,
+        description: process.env.VUE_APP_DESCRIPTION,
+        domain: process.env.VUE_APP_DOMAIN,
+        logo: process.env.VUE_APP_LOGO,
+        contactmail: process.env.VUE_APP_CONTACTMAIL,
+        pagetitle: process.env.VUE_APP_PAGETITLE,
+        supportlink: process.env.VUE_APP_SUPPORTLINK
       }
-    }
-  },
-  mounted() {
-    console.log('Wikisettings')
-  },
-  methods: {
-    saveSettings() {
-      this.$config.name = this.settings.name
-      this.$config.description = this.settings.description
-      this.$writeConfig()
     }
   }
 })
