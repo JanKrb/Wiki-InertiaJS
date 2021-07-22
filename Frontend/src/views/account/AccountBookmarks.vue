a03e83300f2b454dbe073fd4f65e9827cb5f8d0b
Nino Gralla <nino.gralla@gmx.de>
2021-07-22 03:20:57 +0200

    fix Account bookmark paginate error

--- Frontend/src/views/account/AccountBookmarks.vue
+++ Frontend/src/views/account/AccountBookmarks.vue
@@ -99,7 +99,7 @@ export default defineComponent({
     },
     fetchBookmarks(id) {
       const loader = this.$loading.show()
-      axios.get('http://localhost:8000/api/users/' + id + '/bookmarks')
+      axios.get('http://localhost:8000/api/users/' + id + '/bookmarks?paginate=0')
         .then(response => {
           this.bookmarks = response.data.data
           loader.hide()
