648e0f4e2979bea8a24fb7c64b50929502d32bf9
JanKrb <jankrb@jankrb.com>
2021-07-24 14:22:05 +0200

    Fix migration name

--- Backend/database/migrations/2021_06_15_183235_add_issuer_type_to_activity.php
+++ Backend/database/migrations/2021_06_15_183235_add_issuer_type_to_activity.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIssuerTypeToActivities extends Migration
+class AddIssuerTypeToActivity extends Migration
 {
     /**
      * Run the migrations.
