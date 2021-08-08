ebfab1c6a59e59919c1286e79ca068eba4015bfe
JanKrb <jankrb@jankrb.com>
2021-08-08 18:28:02 +0200

    Implement TailwindCSS, Vue3, Babel, Lint other required libs

--- Merged/webpack.mix.js
+++ Merged/webpack.mix.js
@@ -11,7 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss')
-    ]);
+    ])
+
+    .js('resources/js/app.js', 'public/js')
+
+    .version();
