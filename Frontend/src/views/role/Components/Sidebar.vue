516359672f6fd61a7bd1d0af47097c38ca0bbaf1
Nino Gralla <nino.gralla@gmx.de>
2021-08-26 03:02:00 +0200

    add name field to role sidebar

--- Frontend/src/views/role/Components/Sidebar.vue
+++ Frontend/src/views/role/Components/Sidebar.vue
@@ -1,34 +1,39 @@
 <template>
-  <div class="col-span-12 lg:col-span-3">
-    <div class="box p-5">
-      <div class="flex items-center border-b border-gray-200 dark:border-dark-5 pb-5">
-        <div>
-          <div class="text-gray-600">Role ID</div>
-          <div class="mt-1">{{ this.role?.id ?? 'Unknown' }}</div>
-        </div>
-        <ShieldIcon class="w-4 h-4 text-gray-600 ml-auto" />
+  <div class="box p-5">
+    <div class="flex items-center border-b border-gray-200 dark:border-dark-5 pb-5">
+      <div>
+        <div class="text-gray-600">Role ID</div>
+        <div class="mt-1">{{ this.role?.id ?? 'Unknown' }}</div>
       </div>
-      <div class="flex items-center border-b border-gray-200 dark:border-dark-5 py-5">
-        <div>
-          <div class="text-gray-600">Created by</div>
-          <div class="mt-1">{{ this.role?.user?.name ?? 'Unknown' }}</div>
-        </div>
-        <UserIcon class="w-4 h-4 text-gray-600 ml-auto" />
+      <HashIcon class="w-4 h-4 text-gray-600 ml-auto" />
+    </div>
+    <div class="flex items-center border-b border-gray-200 dark:border-dark-5 py-5">
+      <div>
+        <div class="text-gray-600">Role Name</div>
+        <div class="mt-1">{{ this.role?.name ?? 'Unknown' }}</div>
       </div>
-      <div class="flex items-center border-b border-gray-200 dark:border-dark-5 py-5">
-        <div>
-          <div class="text-gray-600">Created at</div>
-          <div class="mt-1">{{ this.role.created_at ? formatDate(this.role.created_at) : 'Unknown' }}</div>
-        </div>
-        <PlusCircleIcon class="w-4 h-4 text-gray-600 ml-auto" />
+      <ShieldIcon class="w-4 h-4 text-gray-600 ml-auto" />
+    </div>
+    <div class="flex items-center border-b border-gray-200 dark:border-dark-5 py-5">
+      <div>
+        <div class="text-gray-600">Created by</div>
+        <div class="mt-1">{{ this.role?.user?.name ?? 'Unknown' }}</div>
       </div>
-      <div class="flex items-center pt-5">
-        <div>
-          <div class="text-gray-600">Last Update</div>
-          <div class="mt-1">{{ this.role.updated_at ? formatDate(this.role.updated_at) : 'Unknown' }}</div>
-        </div>
-        <ClockIcon class="w-4 h-4 text-gray-600 ml-auto" />
+      <UserIcon class="w-4 h-4 text-gray-600 ml-auto" />
+    </div>
+    <div class="flex items-center border-b border-gray-200 dark:border-dark-5 py-5">
+      <div>
+        <div class="text-gray-600">Created at</div>
+        <div class="mt-1">{{ this.role.created_at ? formatDate(this.role.created_at) : 'Unknown' }}</div>
       </div>
+      <PlusCircleIcon class="w-4 h-4 text-gray-600 ml-auto" />
+    </div>
+    <div class="flex items-center pt-5">
+      <div>
+        <div class="text-gray-600">Last Update</div>
+        <div class="mt-1">{{ this.role.updated_at ? formatDate(this.role.updated_at) : 'Unknown' }}</div>
+      </div>
+      <ClockIcon class="w-4 h-4 text-gray-600 ml-auto" />
     </div>
   </div>
 </template>
